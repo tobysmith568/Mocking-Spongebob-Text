cd6c2c2d4b4fc84d022f5715fd990c715fa5bfe3
Toby Smith <tobysmith568@hotmail.co.uk>
2019-09-05 03:40:51 +0100

    Implemented mocking of keyboard content

--- MockingSpongebobText/MainWindow.xaml.cs
+++ MockingSpongebobText/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Gma.System.MouseKeyHook;
+using Clipboard = System.Windows.Forms.Clipboard;
 
 namespace MockingSpongebobText
 {
@@ -54,11 +55,20 @@ namespace MockingSpongebobText
             e.Cancel = true;
         }
 
+        /// <exception cref="System.Runtime.InteropServices.ExternalException"></exception>
+        /// <exception cref="System.Threading.ThreadStateException"></exception>
         private void KeyboardMouseEvents_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
         {
             if (e.Shift && e.Alt && e.KeyCode == Keys.S)
             {
+                if (Clipboard.ContainsText())
+                {
+                    string content = Clipboard.GetText();
 
+                    content = MockifyText(content);
+
+                    Clipboard.SetText(content);
+                }
             }
         }
 
@@ -68,6 +78,23 @@ namespace MockingSpongebobText
         private void DoIconLeftClick()
         {
             Visibility = Visibility.Visible;
+        }
+
+        private string MockifyText(string input)
+        {
+            if (input == null)
+            {
+                return string.Empty;
+            }
+
+            string result = string.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                result += i % 2 == 0 ? char.ToUpper(input[i]) : char.ToLower(input[i]);
+            }
+
+            return result;
         }
     }
 }

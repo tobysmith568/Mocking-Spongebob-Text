387dd6a14b1e1a4f0f454d2c29b5bc5c50520a0e
Toby Smith <tobysmith568@hotmail.co.uk>
2019-09-05 01:44:18 +0100

    Implemented the system tray icon

---
+++ MockingSpongebobText/Command.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MockingSpongebobText
+{
+    public class Command : Command<object>
+    {
+        //  Constructors
+        //  ============
+
+        public Command(Action action, bool canExecute = true) : base(action, canExecute)
+        {
+        }
+
+        public Command(Action<object> parameterizedAction, bool canExecute = true) : base(parameterizedAction, canExecute)
+        {
+        }
+    }
+
+    public class Command<T> : ICommand
+    {
+        //  Variables
+        //  =========
+
+        protected Action action = null;
+        protected Action<T> parameterizedAction = null;
+        public event EventHandler CanExecuteChanged;
+
+        private bool canExecute = false;
+
+        //  Properties
+        //  ==========
+
+        public bool CanExecute
+        {
+            get => canExecute;
+            set
+            {
+                if (canExecute != value)
+                {
+                    canExecute = value;
+                    CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        //  Constructors
+        //  ============
+
+        public Command(Action action, bool canExecute = true)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        public Command(Action<T> parameterizedAction, bool canExecute = true)
+        {
+            this.parameterizedAction = parameterizedAction;
+            this.canExecute = canExecute;
+        }
+
+        //  Methods
+        //  =======
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return canExecute;
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            DoExecute(parameter);
+        }
+
+        public virtual void DoExecute(object param)
+        {
+            InvokeAction(param == null ? default : ((T)param));
+        }
+
+        protected void InvokeAction(T param)
+        {
+            Action theAction = action;
+            Action<T> theParameterizedAction = parameterizedAction;
+            if (theAction != null)
+            {
+                theAction();
+            }
+            else
+            {
+                theParameterizedAction?.Invoke(param);
+            }
+        }
+    }
+}
--- MockingSpongebobText/MainWindow.xaml.cs
+++ MockingSpongebobText/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +20,37 @@ namespace MockingSpongebobText
     /// </summary>
     public partial class MainWindow : Window
     {
+        //  Properties
+        //  ==========
+
+        public Command IconLeftClick { get; set; }
+
+        //  Constructors
+        //  ============
+
         public MainWindow()
         {
             InitializeComponent();
+
+            IconLeftClick = new Command(DoIconLeftClick);
+
+        }
+
+        //  Events
+        //  ======
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Visibility = Visibility.Hidden;
+            e.Cancel = true;
+        }
+
+        //  Methods
+        //  =======
+
+        private void DoIconLeftClick()
+        {
+            Visibility = Visibility.Visible;
         }
     }
 }
